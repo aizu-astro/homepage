ffcd5125594438bc1c0245d22f3fece07a49e126
aizu-astro <aizuastro@gmail.com>
2021-10-26 12:30:00 +0900

    [1] first commit

---
+++ for_member/script.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#for_member',
+    data: {
+        hamburger: false,
+        url_twitter: 'https://twitter.com/aizu_astro',
+        url_instagram: 'https://instagram.com/aizu_astro',
+        url_kibousya: 'https://docs.google.com/forms/d/1yEKQDrH2Rg3jT3xBfc6GRevokw228_ZYlrcY2LzV1sw/edit',
+        mail_address: 'mailto:s1280167@u-aizu.ac.jp',
+
+    },
+    methods: {
+        onHamburger: function() {
+            hamburger = !hamburger;
+        },
+    }
+})
